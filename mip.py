3a686aa01a9b7fd4d3ce6bc95625ccc7ee6648ca
david-hoffman <dave.p.hoffman@gmail.com>
2015-06-18 08:23:24 -0400

    first commit

---
+++ mip.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""mip
+
+A utility to call mip from the command line
+
+Usage:
+  mip.py <myfile> [--PDF]
+  mip.py -h | --help
+
+Options:
+  -h --help     Show this screen.
+  --PDF         Print PDF
+
+"""
+
+from docopt import docopt
+import matplotlib.pyplot as plt
+from dphplotting.mip import mip
+
+if __name__=='__main__': #check to see if we're being run from the command line
+    #if we want to update this to take more arguments we'll need to use one of the
+    #argument parsing packages
+
+    #grab the arguments from the command line
+    arg = docopt(__doc__)
+
+    #a little output so that the user knows whats going on
+    print('Running mip on',arg['<myfile>'])
+
+    #Need to take the first system argument as the filename for a TIF file
+
+    #test if filename has tiff in it
+    filename = arg['<myfile>']
+
+    #try our main block
+    try:
+        if '.tif' in filename or '.tiff' in filename:
+            #Import skimage so we have access to tiff loading
+            from skimage.external import tifffile as tif
+
+            #here's the real danger zone, did the user give us a real file?
+            try:
+                data = tif.imread(filename)
+            except FileNotFoundError as er:
+                raise er
+
+            #plot the data
+            fig, ax = mip(data)
+            #readjust the white space (maybe move this into main code later)
+            fig.subplots_adjust(top=0.85,hspace=0.3,wspace=0.3)
+            #add an overall figure title that's the file name
+            fig.suptitle(filename, fontsize=16)
+
+            #check to see if we should make a PDF
+            if arg['--PDF']:
+                fig.savefig(filename.replace('.tiff','.pdf').replace('.tif','.pdf'))
+            else:
+                #I still don't know why fig.show() doesn't work
+                #I think it has something to do with pyplot's backend
+                plt.show()
+        else:
+            #this is our own baby error handling, it avoids loading the
+            #skimage package
+            print('You didn\'t give me a TIFF')
+
+    except Exception as er:
+        print(er)
