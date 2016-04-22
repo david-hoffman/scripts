71dd096b1d60063ef3b297c6f16ef496174d9e57
David Hoffman <dave.p.hoffman@gmail.com>
2016-04-22 11:10:45 -0400

    sensible variable names

--- rpcaADMM.py
+++ rpcaADMM.py
@@ -59,16 +59,16 @@ def avg(*args):
     return x
 
 
-def objective(X_1, g_2, X_2, g_3, X_3):
+def objective(error, g_2, sparse, g_3, lowrank):
     """
     Objective function for Robust PCA:
         Noise - squared frobenius norm (makes X_i small)
         Background - nuclear norm (makes X_i low rank)
         Foreground - entrywise L1 norm (makes X_i small)
     """
-    tmp = svd(X_3, compute_uv=0)
+    tmp = svd(lowrank, compute_uv=0)
     tmp = tmp.reshape((len(tmp), 1))
-    return norm(X_1, 'fro')**2 + g_2*norm(hstack(X_2), 1) + g_3*norm(tmp, 1)
+    return norm(error, 'fro')**2 + g_2*norm(hstack(sparse), 1) + g_3*norm(tmp, 1)
 
 
 def rpcaADMM(data, lambdap=1.0, MAX_ITER=1000, ABSTOL=1e-4, RELTOL=1e-2):
@@ -95,9 +95,9 @@ def rpcaADMM(data, lambdap=1.0, MAX_ITER=1000, ABSTOL=1e-4, RELTOL=1e-2):
 
     rho = 1.0/lambdap
 
-    X_1 = zeros((m, n))
-    X_2 = zeros((m, n))
-    X_3 = zeros((m, n))
+    error = zeros((m, n))
+    sparse = zeros((m, n))
+    lowrank = zeros((m, n))
     z   = zeros((m, N*n))
     U   = zeros((m, n))
 
@@ -116,11 +116,11 @@ def rpcaADMM(data, lambdap=1.0, MAX_ITER=1000, ABSTOL=1e-4, RELTOL=1e-2):
     h['eps_pri'] = zeros(MAX_ITER)
     h['eps_dual'] = zeros(MAX_ITER)
 
-    def x1update(x, b, l):
+    def errorupdate(x, b, l):
         return (1.0/(1.0+l))*(x - b)
-    def x2update(x, b, l, g, pl):
+    def sparseupdate(x, b, l, g, pl):
         return pl(x - b, l*g)
-    def x3update(x, b, l, g, pl, pm):
+    def lowrankupdate(x, b, l, g, pl, pm):
         return pm(x - b, l*g, pl)
 
     def update(func, item):
@@ -129,32 +129,32 @@ def rpcaADMM(data, lambdap=1.0, MAX_ITER=1000, ABSTOL=1e-4, RELTOL=1e-2):
 
     for k in range(MAX_ITER):
 
-        B = avg(X_1, X_2, X_3) - A/N + U
+        B = avg(error, sparse, lowrank) - A/N + U
 
         # Original MATLAB x-update
-        # X_1 = (1.0/(1.0+lambdap))*(X_1 - B)
-        # X_2 = prox_l1(X_2 - B, lambdap*g2)
-        # X_3 = prox_matrix(X_3 - B, lambdap*g3, prox_l1)
+        # error = (1.0/(1.0+lambdap))*(error - B)
+        # sparse = prox_l1(sparse - B, lambdap*g2)
+        # lowrank = prox_matrix(lowrank - B, lambdap*g3, prox_l1)
 
         # Parallel x-update
-        async_X1 = pool.apply_async(update, (lambda x: x1update(x, B, lambdap), X_1))
-        async_X2 = pool.apply_async(update, (lambda x: x2update(x, B, lambdap, g2, prox_l1), X_2))
-        async_X3 = pool.apply_async(update, (lambda x: x3update(x, B, lambdap, g3, prox_l1, prox_matrix), X_3))
+        async_error = pool.apply_async(update, (lambda x: errorupdate(x, B, lambdap), error))
+        async_sparse = pool.apply_async(update, (lambda x: sparseupdate(x, B, lambdap, g2, prox_l1), sparse))
+        async_lowrank = pool.apply_async(update, (lambda x: lowrankupdate(x, B, lambdap, g3, prox_l1, prox_matrix), lowrank))
 
-        X_1 = async_X1.get()
-        X_2 = async_X2.get()
-        X_3 = async_X3.get()
+        error = async_error.get()
+        sparse = async_sparse.get()
+        lowrank = async_lowrank.get()
 
         # (for termination checks only)
-        x = hstack([X_1, X_2, X_3])
+        x = hstack([error, sparse, lowrank])
         zold = z
-        z = x + tile(-avg(X_1, X_2, X_3) + A * 1.0 / N, (1, N))
+        z = x + tile(-avg(error, sparse, lowrank) + A * 1.0 / N, (1, N))
 
         # u-update
         U = B
 
         # diagnostics, reporting, termination checks
-        h['objval'][k] = objective(X_1, g2, X_2, g3, X_3)
+        h['objval'][k] = objective(error, g2, sparse, g3, lowrank)
         h['r_norm'][k] = norm(x - z, 'fro')
         h['s_norm'][k] = norm(-rho*(z - zold), 'fro')
         h['eps_pri'][k] = sqrt(m*n*N)*ABSTOL + RELTOL*maximum(norm(x, 'fro'), norm(-z, 'fro'))
@@ -172,8 +172,8 @@ def rpcaADMM(data, lambdap=1.0, MAX_ITER=1000, ABSTOL=1e-4, RELTOL=1e-2):
 
     h['admm_toc'] = time.time() - start
     h['admm_iter'] = k
-    h['X1_admm'] = X_1
-    h['X2_admm'] = X_2
-    h['X3_admm'] = X_3
+    h['error_admm'] = error
+    h['sparse_admm'] = sparse
+    h['lowrank_admm'] = lowrank
 
     return h
